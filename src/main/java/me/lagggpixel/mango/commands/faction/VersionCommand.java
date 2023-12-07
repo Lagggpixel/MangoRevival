413fa0d694ca1942a6094013f269e716242d70b8
Lagggpixel <lagggpixel@gmail.com>
2023-12-07 18:44:04 +0000

    feat: updated version command

--- src/main/java/me/lagggpixel/mango/commands/faction/VersionCommand.java
+++ src/main/java/me/lagggpixel/mango/commands/faction/VersionCommand.java
@@ -8,7 +8,7 @@ import me.lagggpixel.mango.factions.FactionManager;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
-import java.util.Arrays;
+import java.util.List;
 
 
 public class VersionCommand extends FactionSubCommand {
@@ -17,15 +17,15 @@ public class VersionCommand extends FactionSubCommand {
   private final FactionManager fm = Mango.getInstance().getFactionManager();
 
   public VersionCommand() {
-    super("version", Arrays.asList("ver"));
+    super("version", List.of("ver"));
   }
 
 
   public void execute(Player p, String[] args) {
     sendMessage(p, "&6&m----------------------------------");
     sendMessage(p, "&6This server is running &eMango Version " + Mango.getInstance().getDescription().getVersion() + "&6.");
-    sendMessage(p, "&6This plugin was created by &eAlexandeh &6(&ehttps://github.com/Alexandeh/&6).");
-    sendMessage(p, "&6For more information regarding &eMango&6, visit &ehttp://mango.spookee.me&6.");
+    sendMessage(p, "&6This plugin was originally created by &eAlexandeh &6(&ehttps://github.com/Alexandeh/&6).");
+    sendMessage(p, "&6This plugin is currently maintained by Lagggpixel &6(&ehttps://github.com/Lagggpixel/&6).");
     sendMessage(p, "&6&m----------------------------------");
   }
 
