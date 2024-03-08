36f55e224688440f584990bc37e96115f03c7f77
Wanbin Cao <lagggpixel@gmail.com>
2024-03-08 22:14:15 +0000

    feat: updated classes logic
    Took 9 minutes

--- src/main/java/me/lagggpixel/mango/enums/Classes.java
+++ src/main/java/me/lagggpixel/mango/enums/Classes.java
@@ -1,9 +1,61 @@
 package me.lagggpixel.mango.enums;
 
+import lombok.Getter;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.Map;
+
+@Getter
 public enum Classes {
-  DIAMOND,
-  BARD,
-  ARCHER,
-  ROUGE,
-  MINER
+  DIAMOND(Map.of()),
+  BARD(Map.of(PotionEffectType.SPEED, 2, PotionEffectType.REGENERATION, 1, PotionEffectType.DAMAGE_RESISTANCE, 1)),
+  ARCHER(Map.of(PotionEffectType.SPEED, 3)),
+  ROUGE(Map.of(PotionEffectType.SPEED, 2, PotionEffectType.REGENERATION, 1, PotionEffectType.DAMAGE_RESISTANCE, 1)),
+  MINER(Map.of(PotionEffectType.FAST_DIGGING, 2, PotionEffectType.REGENERATION, 1, PotionEffectType.DAMAGE_RESISTANCE, 1, PotionEffectType.FIRE_RESISTANCE, 1));
+
+  private final Map<PotionEffectType, Integer> classEffects;
+
+  Classes(Map<PotionEffectType, Integer> classEffects) {
+    this.classEffects = classEffects;
+  }
+
+  public static Classes getByName(String name) {
+    if (name.equalsIgnoreCase("DIAMOND")) {
+      return DIAMOND;
+    } else if (name.equalsIgnoreCase("BARD")) {
+      return BARD;
+    } else if (name.equalsIgnoreCase("ARCHER")) {
+      return ARCHER;
+    } else if (name.equalsIgnoreCase("ROUGE")) {
+      return ROUGE;
+    } else if (name.equalsIgnoreCase("MINER")) {
+      return MINER;
+    }
+    return null;
+  }
+
+  public static Classes getClassByArmourSet(ItemStack helmet, ItemStack chestplate, ItemStack leggings, ItemStack boots) {
+    if (helmet == null || chestplate == null || leggings == null || boots == null) {
+      return null;
+    }
+    Material helmetMaterial = helmet.getType();
+    Material chestplateMaterial = chestplate.getType();
+    Material leggingsMaterial = leggings.getType();
+    Material bootsMaterial = boots.getType();
+    if (helmetMaterial == Material.LEATHER_HELMET && chestplateMaterial == Material.LEATHER_CHESTPLATE && leggingsMaterial == Material.LEATHER_LEGGINGS && bootsMaterial == Material.LEATHER_BOOTS) {
+      return Classes.ARCHER;
+    } else if (helmetMaterial == Material.IRON_HELMET && chestplateMaterial == Material.IRON_CHESTPLATE && leggingsMaterial == Material.IRON_LEGGINGS && bootsMaterial == Material.IRON_BOOTS) {
+      return Classes.MINER;
+    } else if (helmetMaterial == Material.GOLDEN_HELMET && chestplateMaterial == Material.GOLDEN_CHESTPLATE && leggingsMaterial == Material.GOLDEN_LEGGINGS && bootsMaterial == Material.GOLDEN_BOOTS) {
+      return Classes.BARD;
+    } else if (helmetMaterial == Material.DIAMOND_HELMET && chestplateMaterial == Material.DIAMOND_CHESTPLATE && leggingsMaterial == Material.DIAMOND_LEGGINGS && bootsMaterial == Material.DIAMOND_BOOTS) {
+      return Classes.DIAMOND;
+    } else if (helmetMaterial == Material.GOLDEN_HELMET && chestplateMaterial == Material.CHAINMAIL_CHESTPLATE && leggingsMaterial == Material.CHAINMAIL_LEGGINGS && bootsMaterial == Material.GOLDEN_BOOTS) {
+      return Classes.ROUGE;
+    }
+
+    return null;
+  }
 }
--- src/main/java/me/lagggpixel/mango/runnable/ClassesRunnable.java
+++ src/main/java/me/lagggpixel/mango/runnable/ClassesRunnable.java
@@ -4,7 +4,6 @@ import me.lagggpixel.mango.Mango;
 import me.lagggpixel.mango.enums.Classes;
 import me.lagggpixel.mango.events.PlayerClassChangeEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -53,39 +52,11 @@ public class ClassesRunnable extends BukkitRunnable implements Listener {
     ItemStack leggings = playerInventory.getLeggings();
     ItemStack boots = playerInventory.getBoots();
 
-    if (helmet == null || chestplate == null || leggings == null || boots == null) {
-      if (previousClass != null) {
-        playerClasses.put(player, null);
-        Bukkit.getServer().getPluginManager().callEvent(new PlayerClassChangeEvent(player, previousClass, null));
-      }
-      return;
-    }
-    if (!isSameArmourMaterial(helmet, chestplate, leggings, boots)) {
-      return;
-    }
-    Material material = getArmourMaterial(helmet);
-    if (material == null) {
-      return;
-    }
-    Classes newClass = switch (material) {
-      case LEATHER -> Classes.ARCHER;
-      case IRON_INGOT -> Classes.MINER;
-      case GOLD_INGOT -> Classes.BARD;
-      case DIAMOND -> Classes.DIAMOND;
-      case FIRE -> Classes.ROUGE;
-      default -> null;
-    };
-    if (newClass == null) {
-      if (previousClass != null) {
-        playerClasses.put(player, null);
-        Bukkit.getServer().getPluginManager().callEvent(new PlayerClassChangeEvent(player, previousClass, null));
-      }
-      return;
-    }
+    Classes newClass = Classes.getClassByArmourSet(helmet, chestplate, leggings, boots);
     if (previousClass == newClass) {
       return;
     }
-    playerClasses.put(player, newClass);
+    playerClasses.replace(player, newClass);
     Bukkit.getServer().getPluginManager().callEvent(new PlayerClassChangeEvent(player, previousClass, newClass));
   }
 
@@ -94,82 +65,5 @@ public class ClassesRunnable extends BukkitRunnable implements Listener {
       return null;
     }
     return playerClasses.get(player);
-  }
-
-  private boolean isSameArmourMaterial(ItemStack helmet, ItemStack chestplate, ItemStack leggings, ItemStack boots) {
-    Material material = getArmourMaterial(helmet);
-    return material != getArmourMaterial(chestplate) && material == getArmourMaterial(leggings) && material == getArmourMaterial(boots);
-  }
-
-  private Material getArmourMaterial(ItemStack itemStack) {
-    Material material = itemStack.getType();
-
-    if (material == Material.LEATHER_HELMET) {
-      return Material.LEATHER;
-    }
-    if (material == Material.LEATHER_CHESTPLATE) {
-      return Material.LEATHER;
-    }
-    if (material == Material.LEATHER_LEGGINGS) {
-      return Material.LEATHER;
-    }
-    if (material == Material.LEATHER_BOOTS) {
-      return Material.LEATHER;
-    }
-
-    if (material == Material.IRON_HELMET) {
-      return Material.IRON_INGOT;
-    }
-    if (material == Material.IRON_CHESTPLATE) {
-      return Material.IRON_INGOT;
-    }
-    if (material == Material.IRON_LEGGINGS) {
-      return Material.IRON_INGOT;
-    }
-    if (material == Material.IRON_BOOTS) {
-      return Material.IRON_INGOT;
-    }
-
-    if (material == Material.GOLDEN_HELMET) {
-      return Material.GOLD_INGOT;
-    }
-    if (material == Material.GOLDEN_CHESTPLATE) {
-      return Material.GOLD_INGOT;
-    }
-    if (material == Material.GOLDEN_LEGGINGS) {
-      return Material.GOLD_INGOT;
-    }
-    if (material == Material.GOLDEN_BOOTS) {
-      return Material.GOLD_INGOT;
-    }
-
-    if (material == Material.DIAMOND_HELMET) {
-      return Material.DIAMOND;
-    }
-    if (material == Material.DIAMOND_CHESTPLATE) {
-      return Material.DIAMOND;
-    }
-    if (material == Material.DIAMOND_LEGGINGS) {
-      return Material.DIAMOND;
-    }
-    if (material == Material.DIAMOND_BOOTS) {
-      return Material.DIAMOND;
-    }
-
-    if (material == Material.CHAINMAIL_HELMET) {
-      return Material.FIRE;
-    }
-    if (material == Material.CHAINMAIL_CHESTPLATE) {
-      return Material.FIRE;
-    }
-    if (material == Material.CHAINMAIL_LEGGINGS) {
-      return Material.FIRE;
-    }
-    if (material == Material.CHAINMAIL_BOOTS) {
-      return Material.FIRE;
-    }
-
-    return null;
-
   }
 }
