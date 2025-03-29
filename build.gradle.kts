d9f285abcae36772db507c100d0a9f8619961440
version-bump[github-action] <41898282+version-bump[github-action]@users.noreply.github.com>
2025-03-29 12:29:57 +0000

    Bump version from 1.1.1-SNAPSHOT2 to 1.1.1-SNAPSHOT3

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
 }
 
 group = "me.lagggpixel"
-version = "1.1.1-SNAPSHOT2"
+version = "1.1.1-SNAPSHOT3"
 description = "MangoRevival"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
